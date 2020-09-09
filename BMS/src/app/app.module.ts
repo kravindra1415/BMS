b34c8c4948cc92a4445f80237f0b0131d1a60bb1
kravindra1415 <58881759+kravindra1415@users.noreply.github.com>
2020-09-09 21:27:36 +0530

    sidebar component added

--- BMS/src/app/app.module.ts
+++ BMS/src/app/app.module.ts
@@ -4,11 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CustomerloginComponent } from './customerlogin/customerlogin.component';
+import { AccountcreationComponent } from './accountcreation/accountcreation.component';
+import { TransfermoneyComponent } from './transfermoney/transfermoney.component';
+import { InsuranceComponent } from './insurance/insurance.component';
+import { CheckbookComponent } from './checkbook/checkbook.component';
+import { FixedDepositComponent } from './fixed-deposit/fixed-deposit.component';
+import { ProfileUpdateComponent } from './profile-update/profile-update.component';
+import { TransactionHistoryComponent } from './transaction-history/transaction-history.component';
+import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    CustomerloginComponent,
+    AccountcreationComponent,
+    TransfermoneyComponent,
+    InsuranceComponent,
+    CheckbookComponent,
+    FixedDepositComponent,
+    ProfileUpdateComponent,
+    TransactionHistoryComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
---
+++ BMS/src/app/checkbook/checkbook.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-checkbook',
+  templateUrl: './checkbook.component.html',
+  styleUrls: ['./checkbook.component.css']
+})
+export class CheckbookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
